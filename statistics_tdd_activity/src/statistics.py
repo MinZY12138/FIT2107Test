139a70766a32db3859f6db5d35d9f4a869cea699
Min Zhengyuan <zmin0008@student.monash.edu>
2025-09-05 22:15:31 +0800

    Write the method stub

---
+++ statistics_tdd_activity/src/statistics.py
@@ -0,0 +1,17 @@
+# src/statistics.py
+
+def sum(data):
+    """Return the sum of all numbers in data (do NOT use Python builtins)."""
+    raise NotImplementedError
+
+def mean(data):
+    """Return the average of numbers in data (do NOT use Python builtins)."""
+    raise NotImplementedError
+
+def minimum(data):
+    """Return the minimum value in data (do NOT use Python builtins)."""
+    raise NotImplementedError
+
+def maximum(data):
+    """Return the maximum value in data (do NOT use Python builtins)."""
+    raise NotImplementedError
