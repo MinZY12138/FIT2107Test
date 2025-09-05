90b071b0a8605360df1b6c2a0615aa3eecf98371
Min Zhengyuan <zmin0008@student.monash.edu>
2025-09-05 23:12:56 +0800

    add Calculator class with method stubs

--- test_your_knowledge_topic_4/src/calculator.py
+++ test_your_knowledge_topic_4/src/calculator.py
@@ -1,4 +1,24 @@
-# Student name:
-# Student ID:
+# Student name: Min Zhengyuan
+# Student ID: 34075887
 
-class Calculator(object):
+class Calculator:
+    def __init__(self):
+        self._answer = 0
+
+    def get_answer(self):
+        return None  # stub
+
+    def reset(self):
+        return self  # stub
+
+    def add(self, num):
+        return self  # stub
+
+    def subtract(self, num):
+        return self  # stub
+
+    def multiply(self, num):
+        return self  # stub
+
+    def power(self, num):
+        return self  # stub
