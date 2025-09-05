0a49880df79bef50c28d76c54c938a2b41e73b3d
Min Zhengyuan <zmin0008@student.monash.edu>
2025-09-05 23:22:02 +0800

    add unit tests for Calculator methods

--- test_your_knowledge_topic_4/tests/test_calculator.py
+++ test_your_knowledge_topic_4/tests/test_calculator.py
@@ -1,14 +1,37 @@
-# Student name:
-# Student ID:
+# Student name: Min Zhengyuan
+# Student ID: 34075887
 
 import unittest
 
-from calculator import Calculator
+from src.calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
     def setUp(self):
-        pass
+        self.calc = Calculator()
 
     def test_initial_answer(self):
-        calc = Calculator()
-        self.assertEqual(0, calc.get_answer())
+        self.assertEqual(0, self.calc.get_answer())
+
+    def test_add(self):
+        self.calc.add(5)
+        self.assertEqual(5, self.calc.get_answer())
+
+    def test_subtract(self):
+        self.calc.subtract(3)
+        self.assertEqual(-3, self.calc.get_answer())
+
+    def test_multiply(self):
+        self.calc.add(2).multiply(4)   # (0+2)*4 = 8
+        self.assertEqual(8, self.calc.get_answer())
+
+    def test_power(self):
+        self.calc.add(2).power(3)      # (0+2)^3 = 8
+        self.assertEqual(8, self.calc.get_answer())
+
+    def test_reset(self):
+        self.calc.add(10).reset()
+        self.assertEqual(0, self.calc.get_answer())
+
+if __name__ == "__main__":
+    unittest.main()
