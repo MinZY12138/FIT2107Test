38635614f5a0fce7be46339724168df1616787b7
Min Zhengyuan <zmin0008@student.monash.edu>
2025-09-12 17:53:32 +0800

    add integration tests covering all calculator methods

--- test_your_knowledge_topic_4/tests/test_calculator.py
+++ test_your_knowledge_topic_4/tests/test_calculator.py
@@ -82,5 +82,31 @@ class TestPower(unittest.TestCase):
         self.calc.power(1)  # 7 ** 1 = 7
         self.assertEqual(self.calc.get_answer(), 7)
 
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_initial_answer(self):
+        self.assertEqual(0, self.calc.get_answer())
+
+    def test_add(self):
+        self.calc.add(5)
+        self.assertEqual(5, self.calc.get_answer())
+
+    def test_subtract(self):
+        self.calc.subtract(3)
+        self.assertEqual(-3, self.calc.get_answer())
+
+    def test_multiply(self):
+        self.calc.add(2).multiply(4)   # (0+2)*4 = 8
+        self.assertEqual(8, self.calc.get_answer())
+
+    def test_power(self):
+        self.calc.add(2).power(3)      # (0+2)^3 = 8
+        self.assertEqual(8, self.calc.get_answer())
+
+    def test_reset(self):
+        self.calc.add(10).reset()
+        self.assertEqual(0, self.calc.get_answer())
 if __name__ == "__main__":
     unittest.main()
