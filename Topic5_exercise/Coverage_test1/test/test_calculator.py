e171216a4788aef0fcc312ee507ca3b1b765b283
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-21 01:54:18 +0800

    some test cases for calculator to test the coverage

--- Topic5_exercise/Coverage_test1/test/test_calculator.py
+++ Topic5_exercise/Coverage_test1/test/test_calculator.py
@@ -9,3 +9,32 @@ class TestCalculator(unittest.TestCase):
     def test_initial_answer(self):
         calc = Calculator()
         self.assertEqual(0, calc.get_answer())
+
+    def test_add(self):
+        calc = Calculator()
+        calc.add(10)
+        self.assertEqual(10, calc.get_answer())
+
+    def test_subtract(self):
+        calc = Calculator()
+        calc.add(10)
+        calc.subtract(4)
+        self.assertEqual(6, calc.get_answer())
+
+    def test_multiply(self):
+        calc = Calculator()
+        calc.add(3)
+        calc.multiply(5)
+        self.assertEqual(15, calc.get_answer())
+
+    def test_power(self):
+        calc = Calculator()
+        calc.add(2)
+        calc.power(3)
+        self.assertEqual(8, calc.get_answer())
+
+    def test_reset(self):
+        calc = Calculator()
+        calc.add(100)
+        calc.reset()
+        self.assertEqual(0, calc.get_answer())
