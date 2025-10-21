7e3f0a4fc37f7c2e5efefbd980758136b4c42d1a
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-21 15:25:46 +0800

    write test cases for statisics.py to test coverage.

--- Topic5_exercise/Coverage_test2/test/test_statistics.py
+++ Topic5_exercise/Coverage_test2/test/test_statistics.py
@@ -5,3 +5,26 @@ from src.statistics import *
 class TestStatistics(unittest.TestCase):
     def default_test(self):
         self.assertTrue(True)
+
+    def test_sum(self):
+        data = [1, 2, 3, 4]
+        self.assertEqual(sum(data), 10)
+        self.assertEqual(sum([]), 0)  
+
+    def test_mean(self):
+        data = [2, 4, 6, 8]
+        self.assertEqual(mean(data), 5)
+
+    def test_minimum(self):
+        data = [7, 3, 5, 9, 1]
+        self.assertEqual(minimum(data), 1)
+
+    def test_maximum(self):
+        data = [7, 3, 5, 9, 1]
+        self.assertEqual(maximum(data), 9)
+
+    def test_single_value(self):
+        data = [5]
+        self.assertEqual(minimum(data), 5)
+        self.assertEqual(maximum(data), 5)
+        self.assertEqual(mean(data), 5)
