e514f378ad5224083de8c3216ddcd1b9d821f5a9
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-21 19:51:20 +0800

    List all possible tests and numbering each test.

--- bat/tests/test_mc_dc.py
+++ bat/tests/test_mc_dc.py
@@ -4,6 +4,15 @@ from src.business_logic import can_borrow_carpentry_tool
 
 '''
 Possible tests:
+1: A=F, B=F, C=F, Outcome=F
+2: A=F, B=F, C=T, Outcome=T
+3: A=F, B=T, C=F, Outcome=T
+4: A=F, B=T, C=T, Outcome=T
+5: A=T, B=F, C=F, Outcome=T
+6: A=T, B=F, C=T, Outcome=T
+7: A=T, B=T, C=F, Outcome=T
+8: A=T, B=T, C=T, Outcome=T
+
 
 Possible optimal sets of tests using MC/DC:
 
