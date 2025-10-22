935ad9cdb4b2793d1f4a8b96701add5a78844106
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-22 15:06:24 +0800

    Delete the invaild test of possible tests and change the outcome result.

--- bat/tests/test_mc_dc.py
+++ bat/tests/test_mc_dc.py
@@ -4,14 +4,12 @@ from src.business_logic import can_borrow_carpentry_tool
 
 '''
 Possible tests:
-1: A=F, B=F, C=F, Outcome=F
-2: A=F, B=F, C=T, Outcome=T
-3: A=F, B=T, C=F, Outcome=T
-4: A=F, B=T, C=T, Outcome=T
-5: A=T, B=F, C=F, Outcome=T
-6: A=T, B=F, C=T, Outcome=T
-7: A=T, B=T, C=F, Outcome=T
-8: A=T, B=T, C=T, Outcome=T
+1: A=F, B=F, C=F, Outcome=T
+2: A=F, B=F, C=T, Outcome=F
+3: A=F, B=T, C=F, Outcome=F
+4: A=T, B=F, C=F, Outcome=F
+5: A=T, B=F, C=T, Outcome=F
+6: A=T, B=T, C=F, Outcome=F
 
 
 Possible optimal sets of tests using MC/DC: {1, 2, 3, 5}
