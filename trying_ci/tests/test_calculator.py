b188ea2311fc110fe12aa1d03bc7d6a98997050c
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-23 06:10:03 +0800

    Add docstrings to test_calculator to satisfy pylint

--- trying_ci/tests/test_calculator.py
+++ trying_ci/tests/test_calculator.py
@@ -1,11 +1,17 @@
-import unittest
+"""Unit tests for the Calculator class."""
 
+import unittest
 from src.calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
+    """Tests for the Calculator class."""
+
     def setUp(self):
+        """Set up a Calculator instance for each test."""
         pass
 
     def test_initial_answer(self):
+        """Test that a new Calculator starts with an answer of zero."""
         calc = Calculator()
         self.assertEqual(0, calc.get_answer())
